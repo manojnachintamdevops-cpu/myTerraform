6e97b3f0d72c777777a46797b2193ff2e9c67e0d
manojna chintam <manojnachintam@manojnas-Mac-mini.local>
2025-09-21 16:20:40 -0700

    modified s3.tf

--- s3.tf
+++ s3.tf
@@ -5,13 +5,3 @@ resource "aws_s3_bucket" "backend_s3" {
   }
 
 }
-terraform {
-  backend "s3" {
-    bucket = "manu84bucketterraform-20250918"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-   
-   
-  }
-}
