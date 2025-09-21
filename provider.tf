9db1f5d4d38fe0e02f67dee3180ed3ba01ab7be8
manojna chintam <manojnachintam@manojnas-Mac-mini.local>
2025-09-21 16:19:42 -0700

    modified provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region = "us-east-1"
 
 }
+terraform {
+  backend "s3" {
+    bucket = "manu84bucketterraform-20250918"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+   
+   
+  }
+}
